92243d505db4d553952101b3ff7bda42613ed679
义宇 <yiyu.ljw@taobao.com>
2015-03-27 18:08:18 +0800

    make interceptor early

--- src/index.js
+++ src/index.js
@@ -1,45 +1,40 @@
 angular.module('ngRap', ['opensearchConfig'])
 .config(['$httpProvider', function(httpProvider) {
     if (window.RAP) {
-        httpProvider.interceptors.push('mockInterceptor');
+    	var mockParam = window.location.search.match(/mock=?(\d)?/);
+    	var mode = mockParam ? +mockParam[1] : 0;
+    	RAP.setMode(mode);
+        httpProvider.interceptors.unshift('rapMockInterceptor');
     }
 }])
-.factory('mockInterceptor', [function() {
-    var mockParam = window.location.search.match(/mock=?(\d)?/);
-    var mode = mockParam ? +mockParam[1] : 0;
-    var preUrl = 'http://' + RAP.getHost() + '/mockjsdata/' + RAP.getProjectId();
-
-    if (!mode) { //默认为 仅拦截白名单中的项
-        mode = 3;
-    }
+.factory('rapMockInterceptor', [function() {
     return {
         request: function(config) {
-            var url = config.url;
+        	var mode = RAP.getMode();
             var whiteList = window.RAP ? RAP.getWhiteList() : [];
-            var blackList = window.RAP ? RAP.getBlackList : [];
+            var blackList = window.RAP ? RAP.getBlackList() : [];
+            var url = config.url;
+        	var mockUrl = 'http://' + RAP.getHost() + '/mockjsdata/' + RAP.getProjectId() + url;
 
-            if (mockParam) {
-                switch (mode) {
-                    case 0: //不拦截
-                        break;
-                    case 1: //拦截全部
-                        config.mocked = 1;
-                        config.url = preUrl + url;
-                        break;
-                    case 2: //黑名单中的项不拦截
-                        if(blackList.indexOf(url) == -1){
-                            config.mocked = 2;
-                            config.url = preUrl + url;
-                        }
-                        break;
-                    case 3: //仅拦截白名单中的项
-                        if (whiteList.indexOf(url) != -1) {
-                            config.mocked = 3;
-                            config.url = preUrl + url;
-                            console.log(config.url);
-                        }
-                        break;
-                }
+            switch (mode) {
+                case 0: //不拦截
+                    break;
+                case 1: //拦截全部
+                    config.mocked = true;
+                    config.url = mockUrl;
+                    break;
+                case 2: //黑名单中的项不拦截
+                    if (blackList.indexOf(url) == -1) {
+                        config.mocked = true;
+                        config.url = mockUrl;
+                    }
+                    break;
+                case 3: //仅拦截白名单中的项
+                    if (whiteList.indexOf(url) != -1) {
+                        config.mocked = true;
+                        config.url = mockUrl;
+                    }
+                    break;
             }
             return config;
         }
